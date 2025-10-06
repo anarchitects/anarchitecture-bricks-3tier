d9bba871863e61e6f47c0feb70d1c7c6afc56076
Johan Vrolix <johan@vrolix.eu>
2025-10-06 15:45:07 +0200

    chore: moved to correct folder (typo)

--- libs/common/nest/config/mailer/jest.config.ts
+++ libs/common/nest/config/mailer/jest.config.ts
@@ -6,5 +6,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../../coverage/libs/comon/nest/config/mailer',
+  coverageDirectory: '../../../../../coverage/libs/common/nest/config/mailer',
 };
