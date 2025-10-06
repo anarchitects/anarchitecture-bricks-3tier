352199173b3f552f7b9e06fcbd7971fb4170f4c8
Johan Vrolix <johan@vrolix.eu>
2025-10-06 15:39:18 +0200

    init(common-nest-config-mailer): Initial setup of lib

---
+++ libs/comon/nest/config/mailer/README.md
@@ -0,0 +1,11 @@
+# common-nest-config-mailer
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build common-nest-config-mailer` to build the library.
+
+## Running unit tests
+
+Run `nx test common-nest-config-mailer` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/comon/nest/config/mailer/src/lib/config.ts
@@ -0,0 +1,19 @@
+import { Inject } from '@nestjs/common';
+import { ConfigType, registerAs } from '@nestjs/config';
+
+export const MAILER_CONFIG_KEY = 'mailerConfig';
+
+export const mailerConfig = registerAs(MAILER_CONFIG_KEY, () => ({
+  host: process.env['MAILER_HOST'] ?? 'smtp.example.com',
+  port: parseInt(process.env['MAILER_PORT'] ?? '587', 10),
+  secure: process.env['MAILER_SECURE'] === 'true',
+  user: process.env['MAILER_USER'] ?? 'user@example.com',
+  pass: process.env['MAILER_PASS'] ?? 'password',
+  default: process.env['MAILER_DEFAULT'] ?? 'default@example.com',
+  ignoreTLS: process.env['MAILER_IGNORE_TLS'] === 'true',
+  templateDir: process.env['MAILER_TEMPLATE_DIR'] ?? 'templates',
+}));
+
+export type MailerConfig = ConfigType<typeof mailerConfig>;
+
+export const InjectMailerConfig = () => Inject(mailerConfig.KEY);
