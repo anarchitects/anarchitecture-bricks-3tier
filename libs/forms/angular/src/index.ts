a638ba54b5a96c6acae4246064509541af0f3675
Johan Vrolix <johan@vrolix.eu>
2025-10-19 12:30:56 +0200

    chore(angular-forms): setup build properly

---
+++ libs/forms/angular/src/index.ts
@@ -0,0 +1,5 @@
+export * from '@anarchitects/forms-angular/config';
+export * from '@anarchitects/forms-angular/data-access';
+export * from '@anarchitects/forms-angular/state';
+export * from '@anarchitects/forms-angular/ui';
+export * from '@anarchitects/forms-angular/feature';
