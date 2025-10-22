a05d49961268460cbeb495750ce5da51dcbc6077
Johan Vrolix <johan@vrolix.eu>
2025-10-22 14:56:56 +0200

    feat(forms-nest): create composition in FormsPresentationModule

--- libs/forms/nest/src/presentation/presentation.module.ts
+++ libs/forms/nest/src/presentation/presentation.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { SubmissionsController } from './controllers/submissions.controller';
 import { FormsController } from './controllers/forms.controller';
 import { FormsApplicationModule } from '../application';
+import { FormsPersistenceModule } from '../infrastructure-persistence';
+import { FormsMailerModule } from '../infrastructure-mailer';
 
 @Module({
-  imports: [FormsApplicationModule],
+  imports: [FormsApplicationModule, FormsPersistenceModule, FormsMailerModule],
   controllers: [SubmissionsController, FormsController],
 })
 export class FormsPresentationModule {}
