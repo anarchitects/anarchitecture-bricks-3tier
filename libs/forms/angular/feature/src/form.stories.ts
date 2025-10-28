fc3aa1143850799342711e6f3b6e7be66469aa33
Johan Vrolix <vrxj81@users.noreply.github.com>
2025-10-28 10:43:06 +0100

    Feat/forms-angular-label (#60)
    * fix(forms-angular): Form should show placeholder for checkbox correctly, not label
    
    * Merge pull request #59 from anarchitects:dev/forms-angular-label
    
    fix(forms-angular): Form should show placeholder for checkbox correctly, not label
    
    * chore(release): publish
    
    - project: forms-angular 0.0.3

--- libs/forms/angular/feature/src/form.stories.ts
+++ libs/forms/angular/feature/src/form.stories.ts
@@ -51,7 +51,8 @@ const mockFormConfig: FormConfig = {
       kind: 'boolean',
       required: true,
       ui: {
-        label: 'I agree to the terms and conditions',
+        label: 'Consent',
+        placeholder: 'I agree to the terms and conditions',
       },
     },
   ],
