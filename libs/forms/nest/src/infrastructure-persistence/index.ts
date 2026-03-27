faac5a03d19367bf7a766a52fe5dbd0c9ecee71c
Johan Vrolix <johan@vrolix.eu>
2026-03-27 11:08:03 +0100

    fix(forms-nest): added missing migration export

--- libs/forms/nest/src/infrastructure-persistence/index.ts
+++ libs/forms/nest/src/infrastructure-persistence/index.ts
@@ -1,7 +1,8 @@
-export * from './persistence.module';
-export * from './repositories/submissions.repository';
-export * from './repositories/form-configs.repository';
-export * from './entities/submission.entity';
-export * from './entities/form-config.entity';
-export * from './migrations/1720300000000-create-forms-tables';
 export type { FormsInfrastructurePersistenceModuleOptions } from '../config';
+export * from './entities/form-config.entity';
+export * from './entities/submission.entity';
+export * from './migrations/1720300000000-create-forms-tables';
+export * from './migrations/1720310000000-add-validation-rules-to-form-configs';
+export * from './persistence.module';
+export * from './repositories/form-configs.repository';
+export * from './repositories/submissions.repository';
